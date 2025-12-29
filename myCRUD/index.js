fe85830ad9acf2328a7b0d3636fbe77498d0ef9f
Shyam Mantri <iamshyam1911@gmail.com>
2025-12-29 21:20:01 +0530

    added patch operation

--- myCRUD/index.js
+++ myCRUD/index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const app = express();
 app.use(express.json());
 
-let students = JSON.parse(fs.readFileSync("./data/students.json"));
+let students = JSON.parse(fs.readFileSync("./myCRUD/data/students.json"));
 
 app.get("/cse/get/all", (req, res) => {
   res.json(students);
@@ -22,9 +22,49 @@ app.get("/cse/getbyid/:id", (req, res) => {
 app.post("/cse/post", (req, res) => {
   const newStudent = req.body;
   students.push(newStudent);
-  fs.writeFile("./data/students.json", JSON.stringify(students), (err) => {
-    res.send("DATA_INSERT");
-  });
+  fs.writeFile(
+    "./myCRUD/data/students.json",
+    JSON.stringify(students),
+    (err) => {
+      res.send("DATA_INSERT");
+    }
+  );
+});
+
+app.delete("/cse/deletebyid/:id", (req, res) => {
+  let id = req.params.id;
+  let data = students.find((el) => el.id == id);
+  if (!data) {
+    res.send("No Data found with id");
+  } else {
+    let index = students.indexOf(data);
+    students.splice(index, 1);
+    fs.writeFile(
+      "./myCRUD/data/students.json",
+      JSON.stringify(students),
+      (err) => {
+        res.send("DATA_DELETED");
+      }
+    );
+  }
+});
+
+app.patch("/cse/updatebyid/:id", (req, res) => {
+  let id = req.params.id;
+  let stupdate = students.find((el) => el.id == id);
+  if (!stupdate) {
+    res.send("Not data found with id");
+  } else {
+    let index = students.indexOf(stupdate);
+    Object.assign(stupdate, req.body);
+    fs.writeFile(
+      "./myCRUD/data/students.json",
+      JSON.stringify(students),
+      (err) => {
+        res.send("DATA_UPDATED");
+      }
+    );
+  }
 });
 
 app.listen(3000, (req, res) => {
